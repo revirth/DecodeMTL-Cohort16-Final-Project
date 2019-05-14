c1e8f650b53a7df4da3cfbf89ffae13766baae81
Marat <marat.zinatullin@yahoo.com>
2019-05-14 10:58:58 -0400

    Added a condition that the CartBar is shown if the user is logged in and there is items in their Cart

--- frontend/src/component/cartfooter/CartBar.jsx
+++ frontend/src/component/cartfooter/CartBar.jsx
@@ -11,7 +11,51 @@ class UnconnectedCartBar extends React.Component {
   }
   
   render() {
-    return (
+    let content = ""
+    if (this.props.loggedIn && this.props.number > 0){
+      content = (
+        // generate CartBar page
+        <div className="cartBar">
+          <div className="footer row">
+            <div className="equal-width" />
+            <div className="equal-width wrapper-center">
+              <div className="child-center">
+                <div className="column text-title">Cart :</div>
+                <div className="column number"> {this.props.number}</div>
+                <div className="column">
+                  <div className="height100">
+                    <img
+                      src="https://image.flaticon.com/icons/svg/60/60992.svg"
+                      height="15px"
+                      alt=""
+                    />
+                  </div>
+                  <div className="text">items</div>
+                </div>
+              </div>
+            </div>
+            <div className="column equal-width wrapper-center">
+              <div className="child-center">
+                <div className="column text-title">Total: </div>
+                <div className="column number">${this.props.total.toFixed(2)}</div>
+              </div>
+            </div>
+            <div className="column equal-width wrapper-center">
+              <div className="child-center">
+                <Link to="/cart/">
+                  <button className="view-cart-button" onClick={this.onClickHandle}>
+                    View Cart
+                </button>
+                </Link>
+              </div>
+            </div>
+            <div className="equal-width" />
+          </div>
+        </div>
+      );
+    }
+
+    return content/*(
       // generate CartBar page
       <div className="cartBar">
         <div className="footer row">
@@ -50,7 +94,7 @@ class UnconnectedCartBar extends React.Component {
           <div className="equal-width" />
         </div>
       </div>
-    );
+    );*/
   }
 }
 
@@ -63,7 +107,7 @@ let mapStatetoProps = state => {
       totalPrice + parseFloat(item.itemPrice) * parseInt(item.itemQuantity);
     numberOfItems = numberOfItems + parseInt(item.itemQuantity);
   });
-  return { number: numberOfItems, total: totalPrice };
+  return { number: numberOfItems, total: totalPrice, loggedIn: state.loggedIn };
 };
 
 let CartBar = connect(mapStatetoProps)(UnconnectedCartBar);
