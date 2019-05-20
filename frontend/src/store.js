7471ce1ad8685706e237d6fa2c6a40e26e39033c
JT <revirth@gmail.com>
2019-05-20 13:42:52 -0400

    store.js refactoring

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,46 +1,16 @@
 import { createStore, applyMiddleware, compose } from "redux";
-// import { ApiAiClient } from "api-ai-javascript";
-// const accessToken = "5fa6f64523ef4168b443821a34096c76";
-// const client = new ApiAiClient({ accessToken });
-
-// const ON_MESSAGE = "ON_MESSAGE";
-
-// export const sendMessage = (text, sender = "user") => ({
-//   type: ON_MESSAGE,
-//   payload: { text, sender }
-// });
-
-// const messageMiddleware = () => next => action => {
-//   next(action);
-//   if (action.type === ON_MESSAGE) {
-//     const { text } = action.payload;
-//     client.textRequest(text).then(onSuccess);
-
-//     function onSuccess(response) {
-//       console.log("response", response);
-//       const speech = response.result.fulfillment.speech;
-//       next(sendMessage(speech, "jarvis"));
-//     }
-//   }
-// };
 
 let reducer = (state, action) => {
-  if (action.type === "FillCart") {
-    return {
-      ...state,
-      cartItems: action.cartItems
-    };
-  }
-
-  if (action.type === "ChangePage") {
-    return { ...state };
-  }
-
-  if (action.type === "CheckIfUserValid") {
-    return { ...state, loggedIn: action.isValidUser };
-  }
-
   switch (action.type) {
+    case "FillCart":
+      return {
+        ...state,
+        cartItems: action.cartItems
+      };
+    case "ChangePage":
+      return { ...state };
+    case "CheckIfUserValid":
+      return { ...state, loggedIn: action.isValidUser };
     case "afterLogin":
       return {
         ...state,
@@ -82,17 +52,10 @@ let getCookie = cname => {
   return "";
 };
 
-let isValidUser = async () => {
-  let response = await fetch("/auth/isvalid", { credentials: "include" });
-  let data = await response.json();
-  return data.status;
-};
-
 let store = createStore(
   reducer,
   {
     cartItems: [],
-    // loggedIn: isValidUser() === true,
     loggedIn: false,
     username: getCookie("sid") !== "" ? getCookie("unm") : "",
     usertype: getCookie("sid") !== "" ? getCookie("utp") : ""
